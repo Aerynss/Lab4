d35967626ac0d724beceb73c6f0805deed0d9fd4
Aerynss <grikis.dim2005@gmail.com>
2024-06-05 00:34:37 +0300

    dcv

--- exchange_rate.cpp
+++
@@ -1,9 +0,0 @@
-#include "exchange_rate.h"
-exchange_rate::exchange_rate() {}
-exchange_rate::exchange_rate(bank _bank_, operations _buying_, operations _selling_, address _address_)
-{
-	this->some_bank = _bank_;
-	this->buying = _buying_;
-	this->selling = _selling_;
-	this->some_address = _address_;
-}
--- processing.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "exchange_rate.h"
-double diff(operations a, operations b);
-double process(exchange_rate* array[], int size, int key);
