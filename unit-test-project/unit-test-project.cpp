2be741b669d240646e4e175ec047b761010fd1aa
Aerynss <grikis.dim2005@gmail.com>
2024-06-05 15:50:52 +0300

    dffde

--- unit-test-project/unit-test-project.cpp
+++ unit-test-project/unit-test-project.cpp
@@ -1,75 +1,56 @@
 #include "pch.h"
 #include "CppUnitTest.h"
-#include "..\Lab4\processing.h"
-#include "..\Lab4\exchange_rate.h"
-#include "..\Lab4\exchange_rate.cpp"
-#include "..\Lab4\address.h"
-#include "..\Lab4\bank.h"
-#include "..\Lab4\operations.h"
-#include "..\Lab4\address.cpp"
-#include "..\Lab4\bank.cpp"
-#include "..\Lab4\operations.cpp"
-#pragma warning(disable:4996)
 
+#include "../main-project/exchange_rates.h"
+#include "../main-project/processing.h"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace unittestproject
 {
-	exchange_rate* build_rate(std::string _bank_, double _buying_, double _selling_, char* _address_)
+	exchange_rates* build_rates(float buy, float sell)
 	{
-		exchange_rate* temp = new exchange_rate;
-		temp->some_bank.bank_name = _bank_;
-		temp->buying.some_oper = _buying_;
-		temp->selling.some_oper = _selling_;
-		strcpy(temp->some_address.bank_address, _address_);
-		return temp;
+		exchange_rates* rates = new exchange_rates;
+		rates->exchange.buy = buy;
+		rates->exchange.sell = sell;
+		return rates;
 	}
-
-	//       
-	void delete_rate(exchange_rate* array[], int size)
-	{
-		for (int i = 0; i < size; i++)
-		{
+	void delete_rates(exchange_rates* array[], int size) {
+		for (int i = 0; i < size; ++i) {
 			delete array[i];
 		}
 	}
 
-
 	TEST_CLASS(unittestproject)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1) //     
+		double precision = 1e-6;
+		TEST_METHOD(TestMethod1)
 		{
-			exchange_rate* rates[3];
-			rates[0] = build_rate("Беларусбанк", 10., 2., "г.Витебск пр.Московский д.54"); 
-			rates[1] = build_rate("Беларусбанк", 10., 2.9, "г.Витебск пр.Московский д.54"); 
-			rates[2] = build_rate("Беларусбанк", 9., 2.4, "г.Витебск пр.Московский д.54"); 
-			Assert::AreEqual(7., process(rates, 3, 0));
-			delete_rate(rates, 3);
+			exchange_rates* rates[3];
+			rates[0] = build_rates(3.17, 3.19);
+			rates[1] = build_rates(3.3, 3.34);
+			rates[2] = build_rates(3.2, 3.1);
+			Assert::AreEqual(0.1, (double)process(rates, 3),precision);
+			delete_rates(rates, 3);
 		}
 
-		TEST_METHOD(TestMethod2) //        
+		TEST_METHOD(TestMethod2)
 		{
-			exchange_rate* rates[3];
-			rates[0] = build_rate("Беларусбанк", 10., 2., "г.Витебск ул.Смоленская д.21"); // 15 
-			rates[1] = build_rate("Беларусбанк", 10., 2.9, "г.Витебск ул.Смоленская д.21"); // 19 
-			rates[2] = build_rate("Беларусбанк", 9., 2.4, "г.Витебск ул.Смоленская д.21"); // 14 
-			Assert::AreEqual(19., process(rates, 3, 1));
-			delete_rate(rates, 3);
+			exchange_rates* rates[3];
+			rates[0] = build_rates(2.85, 2.90);
+			rates[1] = build_rates(3.324, 3.326);
+			rates[2] = build_rates(3.3, 3.31);
+			Assert::AreEqual(0.05, (double)process(rates, 3),precision);
+			delete_rates(rates, 3);
 		}
-
-		TEST_METHOD(TestMethod3) //         
+		TEST_METHOD(TestMethod3)
 		{
-			exchange_rate* rates[3];
-			rates[0] = build_rate("Беларусбанк", 10., 2., "г.Витебск ул.Смоленская д.213"); // 11 
-			rates[1] = build_rate("Беларусбанк", 10., 2.9, "г.Витебск ул.Смоленская д.213"); // 11 
-			rates[2] = build_rate("Беларусбанк", 9., 2.4, "г.Витебск ул.Смоленская д.213"); // 10 
-			Assert::AreEqual(6.6, process(rates, 3, 2));
-			delete_rate(rates, 3);
+			exchange_rates* rates[3];
+			rates[0] = build_rates(3.2, 3.4);
+			rates[1] = build_rates(3.3, 3.34);
+			rates[2] = build_rates(3.2, 3.1);
+			Assert::AreEqual(0.2, (double)process(rates, 3),precision);
+			delete_rates(rates, 3);
 		}
-
-		
-
 	};
 }
